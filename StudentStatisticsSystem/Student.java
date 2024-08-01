fbf223d89bbc1809febee6a60b385b7a76da545e
Urmila <Urmila.10@student.scu.edu.au>
2024-08-01 14:22:56 +0500

    Create Main Method
    I create main method and write code to read the filename

--- StudentStatisticsSystem/Student.java
+++ StudentStatisticsSystem/Student.java
@@ -1,33 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Write a description of class Student here.
  *
- * @author (your name)
+ * @author Urmila
  * @version (a version number or a date)
  */
 public class Student
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Student
-     */
-    public Student()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+     public static void main(String[] args)
+     {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the filename: ");
+        String filename = scanner.nextLine();
+     }
 }
